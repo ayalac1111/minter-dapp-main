2e57ba6f4c62159e26d0f5595191245e33cd2d1e
ayalac1111 <ayalac@algonquincollege.com>
2022-04-04 12:15:50 -0500

    Front-end constant changes

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -3,7 +3,7 @@ const chain = 'rinkeby'; // rinkeby or polygon
 
 const welcome_h1 = "Welcome to the CA NFT Project!!";
 const welcome_h2 = "Connect to MetaMask to Get Started";
-const welcome_p = 'The CA NFT Project is a decentralized, open-source project."
+const welcome_p = "The CA NFT Project is a decentralized, open-source project."
 const h1_presale_coming_soon = "NFT Drop Coming Soon!!";
 const h1_presale_mint = "Pre-Sale Minting Open!!";
 const h1_public_mint = "Public Minting Open!!";
